00825ce5021f063930a42de2f2d44bcccc9c46de
nati <natig.88x@gmail.com>
2017-06-18 16:14:42 +0300

    welcome message

--- app/handlers/command.js
+++ app/handlers/command.js
@@ -5,7 +5,7 @@ module.exports = (msg) => {
 
   switch (msg.text) {
     case '/start':
-      bot.sendMessage(chatId, 'Start command');
+      bot.sendMessage(chatId, `Hello ${msg.from.first_name || msg.from.username || ''} 😀 I'm the ICT Expo 2017 Bot 🤖 and I'll be your assistant for the event. Check out the keyboard below for some of the things i can help you with!`);
       break;
     default:
       bot.sendMessage(chatId, 'Unknown command');
