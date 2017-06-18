900de0b7e9993b05bdfd7a73019253f536b8f1a6
nati <natig.88x@gmail.com>
2017-06-18 16:54:24 +0300

    send main menu with welcome message on /start

---
+++ app/constants/keyboards.js
@@ -0,0 +1,7 @@
+module.exports = {
+  main: [
+    ['🎪 Exhibitors', '📅 Schedule'],
+    ['🤓 Speakers', '🎭 Events'],
+    ['✍️ Register', '💸 Sponsors'],
+  ],
+};
--- app/handlers/command.js
+++ app/handlers/command.js
@@ -1,11 +1,16 @@
 const bot = require('../bot');
+const keyboards = require('../constants/keyboards');
 
 module.exports = (msg) => {
   const chatId = msg.chat.id;
 
   switch (msg.text) {
     case '/start':
-      bot.sendMessage(chatId, `Hello ${msg.from.first_name || msg.from.username || ''} 😀 I'm the ICT Expo 2017 Bot 🤖 and I'll be your assistant for the event. Check out the keyboard below for some of the things i can help you with!`);
+      bot.sendMessage(chatId, `Hello ${msg.from.first_name || msg.from.username || ''} 😀 I'm the ICT Expo 🇪🇹 Bot and I'll be your assistant for the event. Check out the keyboard below for some of the things i can help you with!`, {
+        reply_markup: JSON.stringify({
+          keyboard: keyboards.main,
+        }),
+      });
       break;
     default:
       bot.sendMessage(chatId, 'Unknown command');
