e96d107cf8c7d18749d83d3abe45b4416a38f28d
nati <natig.88x@gmail.com>
2017-06-28 08:35:19 +0300

    feat: register
    request phone number to register user for lotto

--- app/expo/register.js
+++ app/expo/register.js
@@ -1,11 +1,13 @@
 const bot = require('../bot');
 
 module.exports = (msg) => {
-  bot.sendMessage(msg.chat.id, 'How old are you?').then((sent) => {
-    const chatId = sent.chat.id;
-    const messageId = sent.message_id;
-    bot.onReplyToMessage(chatId, messageId, (message) => {
-      console.log('User is %s years old', message.text);
-    });
+  bot.sendMessage(msg.chat.id, 'Send your phone number to register for the expo lottery', {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Send 📞', request_contact: true }],
+        ['🔙 Main Menu'],
+      ],
+      resize_keyboard: true,
+    }),
   });
 };
--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
 const bot = require('./bot');
+const config = require('./config');
 const callback = require('./handlers/callback');
 const command = require('./handlers/command');
 const nlp = require('./handlers/nlp');
 
-bot.on('message', (msg) => {
+bot.on('text', (msg) => {
   if (/\/\w+/.exec(msg.text)) command(msg);
   else nlp(msg);
+});
+
+bot.once('contact', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Info saved', {
+    reply_markup: JSON.stringify({
+      keyboard: config.MAIN_KEYBOARD,
+    }),
+  });
 });
 
 bot.on('callback_query', callback);
